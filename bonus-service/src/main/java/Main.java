0eef0be166f36f6169aac1f1f999f2526e46aa45
rahel81 <rahimova81@gmail.com>
2021-01-01 23:01:40 +0300

    fixed #1

--- bonus-service/src/main/java/Main.java
+++ bonus-service/src/main/java/Main.java
@@ -2,9 +2,20 @@ public class Main {
     public static void main(String[] args) {
         BonusService service = new BonusService();
 
+        // подготавливаем данные:
         long amount = 1000_60;
-        boolean registered = true;
+        boolean registered = false;
+        long expected = 10;
 
-        service.calculate(amount, registered);
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        // если true - то PASS
+        // если false - то FAIL
+        boolean passed = expected == actual;
+
+        // выводим результат
+        System.out.println(passed);
     }
 }
